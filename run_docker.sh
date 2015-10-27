2ab11e8cffe899aeaafc2fedf987da3554b4834d
Evan Lezar <evan.lezar@zalando.de>
2015-10-27 13:11:59 +0100

    Provide optional command line arguments to run_docker.sh. This allows for an interactive shell to be launched.

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/developer/.Xauthority -v `pwd`:/home/developer/expresso --net=host expresso
+docker run -ti -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/developer/.Xauthority -v `pwd`:/home/developer/expresso --net=host expresso $*
